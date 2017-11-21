e5b3185532908bc97aff53f0ef777cd3550a35a3
CarelSchw <31961850+CarelSchw@users.noreply.github.com>
2017-11-21 12:02:54 +0100

    commit

--- code/main.py
+++ code/main.py
@@ -1,12 +1,16 @@
 from DatabaseExtractor import read_data
-import sklearn
+from sklearn.linear_model import LogisticRegression
 
 data = read_data("player_attributes", "*")
 
 solver = "saga"
 
 n_samples = 1000
+algorithm_parameters = "solver='saga', multi_class='multinomial'"
 
-x_data=
-y_data =
+x_data = 1
+y_data = 1
 
+
+clf = LogisticRegression(algorithm_parameters)
+clf.fit(x_data, y_data)
