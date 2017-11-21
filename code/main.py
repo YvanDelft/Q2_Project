6da6966edb5a13c712176f675a5e852d4596b4cc
CarelSchw <31961850+CarelSchw@users.noreply.github.com>
2017-11-21 11:18:05 +0100

    start of lr

--- code/main.py
+++ code/main.py
@@ -1,4 +1,12 @@
 from DatabaseExtractor import read_data
+import sklearn
 
-data = read_data("player", "*")
-print(data[2])
+data = read_data("player_attributes", "*")
+
+solver = "saga"
+
+n_samples = 1000
+
+x_data=
+y_data =
+
