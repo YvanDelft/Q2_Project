5bd0da1dfdbd4532e6768f0ac94e074de339edf8
CarelSchw <31961850+CarelSchw@users.noreply.github.com>
2017-11-23 18:31:29 +0100

    minor changes

--- code/main.py
+++ code/main.py
@@ -5,14 +5,15 @@ from OutputGenerator import *
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 from sklearn.svm import LinearSVC
+from sklearn import svm
 
 
 
 # Read in the data of the match and of the players
 x2 = read_data("Match", "home_player_1")[:1000]
-p1_rating = stats_from_id(x2, "overall_rating", 50)
-print(p1_rating)
-print(len(p1_rating))
+p1_rating = stats_from_id(x2, "overall_rating", 70)
+#print(p1_rating)
+#print(len(p1_rating))
 
 
 y = process_output()
@@ -23,16 +24,22 @@ y_train = y[:800]
 x_test = p1_rating[800:1000]
 y_test = y[800:1000]
 
+
 #scaler = StandardScaler()
 
 # Apply linear regression models [MODELS ARE VERY BAD ATM]
 algorithm_parameters = "solver='lbfgs', multi_class='multinomial'"
-clf = OneVsRestClassifier(LogisticRegression())
+clf = OneVsRestClassifier(LinearSVC())
 result1=clf.fit(x_train, y_train).predict(x_test)
 
 otherclf= LogisticRegression()
 result2 = otherclf.fit(x_train,y_train).predict(x_test)
 
+# print(x_train)
+# clf3 = svm.SVC()
+# clf3p = clf3.fit(x_train, y_train)
+# print(clf3p.predict(x_test))
+# print(x_test[-7:])
 print(result1)
 print(result2)
 
