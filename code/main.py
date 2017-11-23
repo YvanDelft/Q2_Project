da0491296e1e63f763c069b63a15b685fd076251
CarelSchw <31961850+CarelSchw@users.noreply.github.com>
2017-11-23 22:11:40 +0100

    very last

--- code/main.py
+++ code/main.py
@@ -8,13 +8,9 @@ from sklearn.svm import LinearSVC
 from sklearn import svm
 
 
-
 # Read in the data of the match and of the players
 x2 = read_data("Match", "home_player_1")[:1000]
 p1_rating = stats_from_id(x2, "overall_rating", [70])
-#print(p1_rating)
-#print(len(p1_rating))
-
 
 y = process_output()
 
@@ -23,9 +19,6 @@ x_train = p1_rating[:800]
 y_train = y[:800]
 x_test = p1_rating[800:1000]
 y_test = y[800:1000]
-
-
-#scaler = StandardScaler()
 
 # Apply linear regression models [MODELS ARE VERY BAD ATM]
 algorithm_parameters = "solver='lbfgs', multi_class='multinomial'"
