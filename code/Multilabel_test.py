c2c14f8a992dda22b39ff7f1fd7b28ac3ce46b97
YvanDelft <31625742+YvanDelft@users.noreply.github.com>
2017-11-30 14:54:38 +0100

    no message

--- code/Multilabel_test.py
+++ code/Multilabel_test.py
@@ -26,19 +26,23 @@ import numpy as np
   -defence-width
 """
 
-x2 = read_data("Match", "home_player_1")[:1000]
+x2 = read_data("Match", "home_player_1")[:5000]
 p1_rating = stats_from_id(x2, "overall_rating", [70])
 
 y = process_output()
 print(p1_rating)
 
 # split into training and test
-x_train = p1_rating[:800]
-y_train = y[:800]
-x_test = p1_rating[800:1000]
-y_test = y[800:1000]
+x2_train = x2[:4700]
+x_train = p1_rating[:4700]
+y_train = y[:4700]
 
-forest = RandomForestClassifier(n_estimators=1000, max_features=None, max_depth= None, min_samples_split= 2)
+x2_test = x2[4700:5000]
+x_test = p1_rating[4700:5000]
+y_test = y[4700:5000]
+
+
+forest = RandomForestClassifier(n_estimators=10000, max_features=None, max_depth= None, min_samples_split= 2, min_samples_leaf=80)
 multi_target_forest = MultiOutputClassifier(forest, n_jobs=1)
 result = multi_target_forest.fit(x_train, y_train).predict(x_test)
 print(y_test)
